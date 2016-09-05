fbc68cdf0ceab5310b0137f4b7ef99ab7dee3b4f
kwoshvick <kwoshvick@gmail.com>
2016-09-05 15:12:46 +0300

    video

--- saveimage.py
+++ saveimage.py
@@ -1,23 +1,11 @@
-import numpy
+import numpy as np
 import cv2
-import os
 
-directory ='./images'
-#os.chdir(directory)
-#print(os.getcwd())
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)
-print("Files in '%s': %s" % (cwd, files))
-print(os.path.cwd())
-
-
-#image = cv2.imread('1.jpg'),0)
-
-
-#cv2.imshow('myWindow',image)
-#myinput = cv2.waitKey(0)
-#if myinput == 27:
-#    cv2.destroyAllWindows()
-#elif myinput == ord('s'):
-#    cv2.imwrite('1.png',image)
-#    cv2.destroyAllWindows()
+img = cv2.imread('4.jpeg',0)
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.imwrite('4.png',img)
+    cv2.destroyAllWindows()
